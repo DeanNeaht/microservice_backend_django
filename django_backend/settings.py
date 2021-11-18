47a0bd036a0eb9f4531a6bf46463db05c4706b56
Nikolay Solodovnikov <snugp123@gmail.com>
2021-11-18 20:53:17 +0300

    Add allowed hosts

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u9%!7tukdhwdkn!8p330tuu8ga9mz7&3as7=az_^120r+!9y94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
